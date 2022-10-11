714c7684fdced54125280521a9a52900f6e0ca7a
Lars Pasman <95089151+LarsPasman@users.noreply.github.com>
2022-10-11 21:26:37 +0000

    lijntjes werken opzich

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var points = []
+//mult is om de lijnen minder snel te laten ronddraaien
+var mult = 0.005
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(30);
+  angleMode(DEGREES);
+  noiseDetail(1);
 
 //loop om de punten vast te stellen 
   //je kan rondspelen met de density van de ballen
- var density = 50
+ var density = 20
  var space = width / density
 
   for(var x=0; x < width; x += space){
@@ -22,8 +26,10 @@ function draw() {
   fill(255)
 
   for (var i = 0; i < points.length; i++){
+
 //map is de verplaatsing van een object
-  var angle = map(noise(points[i].x,points[i].y),0,1,0,720)
+  var angle = map(noise(points[i].x*mult,points[i].y*mult),0,1,0,720)
+    
 //hij maakt een lijn per punt die random naar links of rechts draait
   points[i].add(createVector(cos(angle),sin(angle)))
     ellipse(points[i].x,points[i].y,1)
